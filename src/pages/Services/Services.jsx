92992d3e6f283799370a921631bd7259d30b85fd
ahouston <alexehouston@gmail.com>
2023-06-05 16:42:23 -0500

    list services

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -3,13 +3,31 @@ import './Services.css';
 export default function Services() {
   return (
     <div className="Services animate__animated animate__fadeIn animate__slow">
-        <ul className="animate__animated animate__fadeInUp">
-            <li>Coloring Services</li>
-            <li>Hairstyling Services</li>
-            <li>Hair Treatment Services</li>
-            <li>Facial Services</li>
-            <li>Make Up Services</li>
-        </ul>
+        <div className="animate__animated animate__fadeInUp">
+            <h6>Coloring Services</h6>
+            <ul>
+              <li>Grey Coverage</li>
+              <li>Highlights</li>
+              <li>All-Over Color</li>
+            </ul>
+            <h6>Hairstyling Services</h6>
+            <ul>
+              <li>Haircuts</li>
+              <li>Hairstyling</li>
+            </ul>
+            <h6>Hair Treatment Services</h6>
+            <ul>
+              <li>Standard Perms</li>
+              <li>Straight Perms</li>
+              <li>Brazilian Blowouts</li>
+            </ul>
+            <h6>Facial Services</h6>
+            <ul>
+              <li>Facials</li>
+              <li>Facial Massages</li>
+              <li>Eye Brow Waxing / Tinting</li>
+            </ul>
+        </div>
     </div>
   );
 }
