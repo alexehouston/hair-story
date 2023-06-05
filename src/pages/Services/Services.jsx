39480014fc724edb559877ef1271b5d525ffb57e
ahouston <alexehouston@gmail.com>
2023-06-05 16:48:00 -0500

    update css

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -5,24 +5,24 @@ export default function Services() {
     <div className="Services animate__animated animate__fadeIn animate__slow">
         <div className="animate__animated animate__fadeInUp">
             <h6>Coloring Services</h6>
-            <ul>
+            <ul className="services-ul">
               <li>Grey Coverage</li>
               <li>Highlights</li>
               <li>All-Over Color</li>
             </ul>
             <h6>Hairstyling Services</h6>
-            <ul>
+            <ul className="services-ul">
               <li>Haircuts</li>
               <li>Hairstyling</li>
             </ul>
             <h6>Hair Treatment Services</h6>
-            <ul>
+            <ul className="services-ul">
               <li>Standard Perms</li>
               <li>Straight Perms</li>
               <li>Brazilian Blowouts</li>
             </ul>
             <h6>Facial Services</h6>
-            <ul>
+            <ul className="facials">
               <li>Facials</li>
               <li>Facial Massages</li>
               <li>Eye Brow Waxing / Tinting</li>
