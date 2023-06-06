2d01ac7afb3d667289c456a8d8ada7d20738653a
ahouston <alexehouston@gmail.com>
2023-06-06 11:44:59 -0500

    update nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,7 @@ import './Nav.css';
 export default function Nav({ currentPage, setCurrentPage }) {
   return (
     <div className="Nav">
-        <h1 className="animate__animated animate__fadeInDown" onClick={() => setCurrentPage('home')}>HAIR STORY SALON</h1>
+        <h1 className="animate__animated animate__fadeInDown" onClick={() => setCurrentPage('home')}>HAIR STORY SALON<br /><span>FAYETTEVILLE, NORTH CAROLINA</span></h1>
 
         <ul className="animate__animated animate__fadeInUp">
             <li className="nav-link hover" onClick={() => setCurrentPage('services')}>services</li>
