41000a39659d6cbb4f1fd67d31466f66e5732929
ahouston <alexehouston@gmail.com>
2023-06-08 15:40:53 -0500

    add bg to about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,6 +6,7 @@ export default function About() {
         <p className="animate__animated animate__fadeInUp">In the heart of Fayetteville, North Carolina, Hair Story Salon stands as a premier destination for comprehensive hair services. Owned and operated by Jin, a highly skilled stylist with two decades of experience in the industry, Hair Story Salon is committed to providing top-notch care and attention to every client.
         <br /><br />
         Jin's expertise in hairdressing, combined with a decade of successful salon ownership, sets Hair Story Salon apart as a trusted establishment. Whether you're seeking a trendy haircut, a bold color transformation, or a sophisticated updo, Jin's mastery in the art of hairstyling will exceed your expectations. At Hair Story Salon, Jin's personalized approach ensures that each client receives a tailored experience, leaving with a hairstyle that tells their unique story. Discover the perfect hair journey at Hair Story Salon, where Jin's passion and expertise intertwine to create stunning transformations.</p>
+        <img src="https://pic.onlinewebfonts.com/svg/img_38281.png" alt="Hair Story" / >
     </div>
   );
 }
